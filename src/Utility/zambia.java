760c63594d7288836ec10a788e3c11b50f95bfc7
deception <hankfetic@gmail.com>
2020-08-05 19:19:48 -0500

    Pushed

---
+++ src/Utility/zambia.java
@@ -0,0 +1,12 @@
+package Utility;
+
+public class zambia {
+    public static void main(String[] args) {
+        System.out.println("hanky");
+
+        int num1 = 1;
+
+        System.out.println(num1);
+
+    }
+}
