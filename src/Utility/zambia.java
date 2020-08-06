d38c81a61300fb6225b0a97a05d918edf2bf9427
deception <hankfetic@gmail.com>
2020-08-05 19:23:53 -0500

    pluh

--- src/Utility/zambia.java
+++ src/Utility/zambia.java
@@ -5,8 +5,12 @@ public class zambia {
         System.out.println("hanky");
 
         int num1 = 1;
-
         System.out.println(num1);
+        int num2 = 3;
+        int num3 = 4;
+
+        System.out.println(num1+num2+num3);
+
 
     }
 }
